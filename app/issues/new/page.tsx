920b1782b217e13ad74a17cebc080f6c35e82faf
maaikevp <m.vanpoll@greenmail.net>
2025-08-01 11:47:21 +0200

    Show the issue list

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -31,6 +31,18 @@ const NewIssuePage = () => {
   const [error, setError] = useState ('');
   const [isSubmitting, setSubmitting] = useState(false);
 
+  const onSubmit= handleSubmit(async (data) => {
+          try {
+            setSubmitting(true);
+            await axios.post('/api/issues', data)
+            router.push('/issues');
+          } catch (error) {
+            setSubmitting(false);
+            setError('An unexpected error occurred.');
+          }
+        });
+
+
   return (
 <div className="max-w-xl">
       {error && (
@@ -40,16 +52,7 @@ const NewIssuePage = () => {
       )}
       <form
         className="space-y-3"
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            setSubmitting(true);
-            await axios.post('/api/issues', data);
-            router.push('/issues');
-          } catch (error) {
-            setSubmitting(false);
-            setError('An unexpected error occurred.');
-          }
-        })}
+        onSubmit={onSubmit}
       >
         <TextField.Root placeholder="Title" {...register('title')}/>
         {<ErrorMessage>{errors.title?.message}</ErrorMessage>}        
