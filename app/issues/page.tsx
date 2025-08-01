9f848caff17c4fde600aa972209032606f139559
maaikevp <m.vanpoll@greenmail.net>
2025-08-01 13:34:08 +0200

    Added status colors/badges

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -2,6 +2,7 @@ import { prisma } from '@/prisma/client';
 import { Button, Table } from '@radix-ui/themes'
 import Link from 'next/link'
 import React from 'react'
+import IssueStatusBadge from '../components/IssueStatusBadge';
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
@@ -16,15 +17,15 @@ const IssuesPage = async () => {
 		<Table.Row>
 			<Table.ColumnHeaderCell >Issue</Table.ColumnHeaderCell>
 			<Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
-			<Table.ColumnHeaderCell className='hidden md:table-cell'>Created</Table.ColumnHeaderCell>
+			<Table.ColumnHeaderCell className='hidden :table-cell'>Created</Table.ColumnHeaderCell>
 		</Table.Row>
 	</Table.Header>
 	<Table.Body>
 		   {issues.map(issue => (
       <Table.Row key={issue.id}>
-        <Table.Cell >{issue.title}<div className='block md:hidden'>{issue.status}</div></Table.Cell>		
-			<Table.Cell className='hidden md:table-cell'>{issue.status}</Table.Cell>
-			<Table.Cell className='hidden md:table-cell'>{issue.createdAt.toDateString()}</Table.Cell>
+        <Table.Cell >{issue.title}<div className='block md:hidden'><IssueStatusBadge status={issue.status}/></div></Table.Cell>		
+			<Table.Cell className='hidden sm:table-cell'><IssueStatusBadge status={issue.status}/></Table.Cell>
+			<Table.Cell className='hidden sm:table-cell'>{issue.createdAt.toDateString()}</Table.Cell>
 		</Table.Row>
     ))}
     </Table.Body>
