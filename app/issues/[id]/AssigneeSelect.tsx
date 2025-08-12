aa934e2cd8bf419b4efabbd6c9ee16aedb105f37
maaikevp <m.vanpoll@greenmail.net>
2025-08-12 21:13:17 +0200

    Populating the selector component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -2,10 +2,23 @@
 
 
 import { Flex, Select } from '@radix-ui/themes'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import classnames from "classnames";
+import { User } from '@prisma/client';
+import axios from 'axios';
 
 const AssigneeSelect = () => {
+      const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const {data} = await axios.get<User[]>('/api/users');
+      setUsers(data);
+    }
+
+    fetchUsers();
+  }, [])
+
   return (
     <div className="block">
     <Flex className="block" maxWidth="100%" asChild>   
@@ -14,18 +27,16 @@ const AssigneeSelect = () => {
         <Select.Content>
             <Select.Group>
                 <Select.Label>Suggestions</Select.Label>
-                <Select.Item value="orange">Orange</Select.Item>
-                <Select.Item value="apple">Apple</Select.Item>
-                <Select.Item value="grape" disabled>
-                    Grape
-                </Select.Item>
+                {users.map(user => (
+                    <Select.Item key={user.id} 
+                    value={user.id}>{user.name}</Select.Item> )) }     
             </Select.Group>
-            <Select.Separator />
+            {/* <Select.Separator />
             <Select.Group>
                 <Select.Label>Vegetables</Select.Label>
                 <Select.Item value="carrot">Carrot</Select.Item>
                 <Select.Item value="potato">Potato</Select.Item>
-            </Select.Group>
+            </Select.Group> */}
         </Select.Content>
     </Select.Root>
     </Flex> 
